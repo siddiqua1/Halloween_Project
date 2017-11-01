95f7bc18174f40a123e1570938867a24c5848c55
Manu Varma <varmam@bxscience.edu>
2017-11-01 15:23:25 -0400

    Another version of MazeGenerator. More up to date but seperate since I want to look at the old version still

---
+++ MazeGeneratorV3.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour {
+	//Generates a maze as a 2D array
+
+	public int rows;
+	public int cols;
+	public float scaleOfEachCell;
+	public float heightOfWall;
+
+	private int[,,] maze;
+	//Maze is (rows x cols x 5) where the 5 element array for each cell represents
+	//The 5 element array = [WallUp, WallRight, WallDown, WallLeft, isVisited] where 1 represents true and 0 represents false
+
+	public GameObject floor;
+	public GameObject wall;
+
+	void putMazeInUnity(){
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < cols; c++) {
+				GameObject newFloor = Instantiate (floor, new Vector3 (r*scaleOfEachCell, 0, c*scaleOfEachCell), Quaternion.identity) as GameObject; 
+				if (maze [r,c,0] == 1) {
+					Quaternion q = Quaternion.identity;
+					q.eulerAngles = new Vector3 (0, 180, 90);
+					GameObject gameWall = Instantiate (wall, new Vector3 ((r - scaleOfEachCell / 2)*scaleOfEachCell, heightOfWall/2, c*scaleOfEachCell), q) as GameObject;
+				}
+				if (maze [r,c,1] == 1) {
+					Quaternion q = Quaternion.identity;
+					q.eulerAngles = new Vector3 (0, 270, 90);
+					GameObject gameWall = Instantiate (wall, new Vector3 (r*scaleOfEachCell, heightOfWall/2, c*scaleOfEachCell + scaleOfEachCell*scaleOfEachCell / 2), q) as GameObject;
+				}
+				if (maze [r,c,2] == 1) {
+					Quaternion q = Quaternion.identity;
+					q.eulerAngles = new Vector3 (0, 0, 90);
+					GameObject gameWall = Instantiate (wall, new Vector3 ((r + scaleOfEachCell / 2)*scaleOfEachCell, heightOfWall/2, c*scaleOfEachCell), q) as GameObject;
+				}
+				if (maze [r,c,3] == 1) {
+					Quaternion q = Quaternion.identity;
+					q.eulerAngles = new Vector3 (0, 90, 90);
+					GameObject gameWall = Instantiate (wall, new Vector3 (r*scaleOfEachCell, heightOfWall/2, (c - scaleOfEachCell / 2)*scaleOfEachCell), q) as GameObject;
+				}				
+			}
+		}
+	}
+
+	void initializeMaze(){
+		maze = new int[rows, cols, 5];
+		for (int c = 0; c < cols; c++) {
+			for (int r = 0; r < rows; r++) {
+				if (c == 0)
+					maze [r, 0, 3] = 1;
+				else if (c == cols - 1)
+					maze [r, c, 1] = 1;
+				if (r == 0)
+					maze [r, c, 0] = 1;
+				else if (r == rows - 1)
+					maze [r, c, 2] = 1;
+			}
+		}
+	}
+
+	int[,,] generateMazeWithRecursiveDivision(int[,,] sectionedMaze, int startRowIndex, int endRowIndex, int startColIndex, int endColIndex){
+		//Recursive division
+		//Random row and column for a wall
+		//Put a hole in the wall for each and call function again on the smaller chambers
+		if (startRowIndex == endRowIndex && startColIndex == endColIndex) {
+			return sectionedMaze;
+		}
+		int randColWall = Random.Range (startColIndex, endColIndex - 1); //Put wall right of this column
+		int randRowWall = Random.Range (startRowIndex, endRowIndex - 1); //Put wall below this column
+		for (int i = startColIndex; i < endColIndex + 1; i++) {
+			sectionedMaze [randRowWall, i, 2] = 1;
+			sectionedMaze [randRowWall + 2, i, 0] = 1;
+		}
+		for (int i = startRowIndex; i < endRowIndex + 1; i++) {
+			sectionedMaze [i, randColWall, 1] = 1;
+			sectionedMaze [i, randColWall + 2, 3] = 1;
+		}
+		//1st slit
+		int randColSlit = Random.Range (startColIndex, endColIndex);
+		sectionedMaze [randRowWall, randColSlit, 2] = 0;
+		sectionedMaze [randRowWall + 2, randColSlit, 0] = 0;
+
+		//2nd slit
+		int randRowSlit = Random.Range (startRowIndex, endRowIndex);
+		sectionedMaze [randRowSlit, randColWall, 1] = 0;
+		sectionedMaze [randRowSlit, randColWall + 2, 3] = 0;
+
+		//Getting 3rd slit
+		if (Random.Range (0, 1) == 0) {
+			if (randColSlit > randColWall) {
+				sectionedMaze [randRowWall, Random.Range (startColIndex, randColWall - 1), 2] = 0;
+				sectionedMaze [randRowWall + 2, Random.Range (startColIndex, randColWall - 1), 0] = 0;
+			} else {
+				sectionedMaze [randRowWall, Random.Range (randColWall + 1, endColIndex + 1), 2] = 0;
+				sectionedMaze [randRowWall + 2, Random.Range (randColWall + 1, endColIndex + 1), 0] = 0;
+			}
+			
+		} else {
+			if (randRowSlit > randRowWall) {
+				sectionedMaze [Random.Range (startRowIndex, randRowWall - 1), randColWall, 1] = 0;
+				sectionedMaze [Random.Range (startRowIndex, randRowWall - 1), randColWall + 2, 3] = 0;
+			} else {
+				sectionedMaze [Random.Range (randRowWall + 1, endRowIndex + 1), randColWall, 1] = 0;
+				sectionedMaze [Random.Range (randRowWall + 1, endRowIndex + 1), randColWall + 2, 3] = 0;
+			}
+		}
+		sectionedMaze = generateMazeWithRecursiveDivision (sectionedMaze, startRowIndex, randRowWall, startColIndex, randColWall);
+		sectionedMaze = generateMazeWithRecursiveDivision (sectionedMaze, randRowWall, endRowIndex, startColIndex, randColWall);
+		sectionedMaze = generateMazeWithRecursiveDivision (sectionedMaze, startRowIndex, randRowWall, randColWall, endColIndex);
+		sectionedMaze = generateMazeWithRecursiveDivision (sectionedMaze, randRowWall, endRowIndex, randColWall, endColIndex);
+		return sectionedMaze;
+	}
+
+	void Start(){
+		initializeMaze ();
+		maze = generateMazeWithRecursiveDivision (maze, 0, rows - 1, 0, cols - 1);
+		putMazeInUnity ();
+	}
+
+}
