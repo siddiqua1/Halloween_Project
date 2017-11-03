1b6e94db647f5eaa0d353e1f4720719f17d135a5
unknown <siddiqua@bxscience.edu>
2017-11-03 10:08:50 -0700

    Global variables for the game.

---
+++ Scripts/GlobalVariables.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariables : MonoBehaviour {
+
+    //first is row of mze, second is column, third describes walls around and if traps on the current cell 
+    public static int[,,] maze;
+    
+    //higher int = higher difficulty should NOT be zero, as it wil be a variable affecting the enemy parameters
+    public static int difficulty;
+
+    //
+    public static int level;
+
+    //
+    public static float time;
+
+    //
+    public static float score;
+
+    //
+    public static int monstersPoints;
+
+    //
+    public static float timeMultiplier;
+
+    //
+    public static float levelMultiplier;
+
+    void Update() {
+        score = (timeMultiplier) * time + (levelMultiplier) * level + monstersPoints;
+    }
+
+}
