1201cb8ad7b6d7ee825e28f2e49d207502891b06
Manu Varma <varmam@bxscience.edu>
2017-11-14 10:20:21 -0500

    Will place collectables randomly at different times

---
+++ PlaceRandomCollectablesRandomly.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceRandomCollectablesRandomly : MonoBehaviour {
+
+	//This script will place the random collectables randomly
+
+	void placeRandomly(int item){
+		int x = Random.Range (0, GlobalVariables.maze.GetLength (0));
+		int y = Random.Range (0, GlobalVariables.maze.GetLength (1));
+		//Spawn at maze[x,y]
+	}
+		
+	//For every 30s, an item spawns
+	void Update () {
+		if (GlobalVariables.time % 30 == 0) {
+			placeRandomly (Random.Range (0, 10));
+		}
+	}
+}
