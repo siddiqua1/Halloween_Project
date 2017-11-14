0de1bccdabdca8319c4894185c91f95e09889126
Manu Varma <varmam@bxscience.edu>
2017-11-14 16:14:44 -0500

    Fully done. Spawns random objects every 30s at random places

--- PlaceRandomCollectablesRandomly.cs
+++ PlaceRandomCollectablesRandomly.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class PlaceRandomCollectablesRandomly : MonoBehaviour {
 
+	public float scaleOfEachCell;
+	public GameObject[] items;
+
 	//This script will place the random collectables randomly
 
 	void placeRandomly(int item){
-		int x = Random.Range (0, GlobalVariables.maze.GetLength (0));
-		int y = Random.Range (0, GlobalVariables.maze.GetLength (1));
-		//Spawn at maze[x,y]
+		int r = Random.Range (0, GlobalVariables.maze.GetLength (0));
+		int c = Random.Range (0, GlobalVariables.maze.GetLength (1));
+		GameObject newItem = Instantiate (items[item], new Vector3 (r*scaleOfEachCell, 1, c*scaleOfEachCell), Quaternion.identity) as GameObject; 
 	}
 		
 	//For every 30s, an item spawns
 	void Update () {
 		if (GlobalVariables.time % 30 == 0) {
-			placeRandomly (Random.Range (0, 10));
+			placeRandomly (Random.Range (0, items.Length));
 		}
 	}
 }
