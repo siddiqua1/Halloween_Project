162ccbe4d6205a63385aa602d76d56361377b9c2
Manu Varma <varmam@bxscience.edu>
2017-11-15 22:16:22 -0500

    Have writing and reading to a file done. Need to write the necessary things though

---
+++ SaveFiles.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class SaveFiles : MonoBehaviour {
+
+	[MenuItem("Tools/Write file")]
+	void SaveToText(int saveNumber){
+		string path = "Saves/save" + saveNumber + ".txt";
+		StreamWriter writer = new StreamWriter(path, true);
+		writer.WriteLine("Test");
+		writer.Close();
+	}
+
+	[MenuItem("Tools/Read file")]
+	void LoadFromText(int saveNumber){
+		string path = "Saves/save" + saveNumber + ".txt";
+		StreamReader reader = new StreamReader(path); 
+		string a = reader.ReadToEnd();
+		reader.Close();
+	}
+		
+}
