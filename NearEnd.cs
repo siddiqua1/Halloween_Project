446de2b4ac3cccce553c8c1053705f7626c77242
Manu Varma <varmam@bxscience.edu>
2017-11-19 21:58:57 -0500

    Add what need if the player is within a distance of the end

---
+++ NearEnd.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NearEnd : MonoBehaviour {
+
+	public string playerName;
+
+	void Update () {
+		Vector3 playerPos = GameObject.Find (playerName).transform.position;
+		if ((playerPos - new Vector3 (GlobalVariables.scaleOfEachCell * (GlobalVariables.row - 1), 0, GlobalVariables.scaleOfEachCell * (GlobalVariables.col - 1))).sqrMagnitude < 0.5) {
+
+		}
+	}
+}
